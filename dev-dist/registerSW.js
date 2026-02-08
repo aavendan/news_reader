d5e5439d8b1958d5e11d077a94a4c2621c77120b
Allan <aavendan@gmail.com>
2026-02-08 16:54:11 +0000

    pwa

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/news_reader/dev-sw.js?dev-sw', { scope: '/news_reader/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import { VitePWA } from 'vite-plugin-pwa'
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
@@ -6,9 +7,66 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: "/news_reader/",
+  base: "/news_reader/",
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        id: '/news_reader/',
+        name: 'Lector de noticias',
+        short_name: 'Lector de noticias',
+        description: 'Lector de noticias de El Universo',
+        theme_color: '#D3D1D1',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          },
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            // Intercepta todas las peticiones a esta API (ajusta según necesidad)
+            urlPattern: /^https:\/\/api\.open-meteo\.com\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'open-meteo-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24, // 1 día
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              }
+            }
+          }
+        ]
+      }
+    }),
     legacy()
   ],
   test: {
